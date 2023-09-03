e7b5c55d35f9e1a5831834d2acd6cf6278e82d50
Christian <flamelessowo@gmail.com>
2023-09-03 23:55:54 +0300

    base cmake for project

---
+++ src/argv/opts.cc
@@ -0,0 +1,7 @@
+namespace opts {
+	
+	class OptParser {
+
+	};
+	
+}
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,9 @@
 #include <cstdio>
-int main (int argc, char *argv[]) {
+#include <iostream>
+#include "./argv/opts.cc"
+int main (int argc, char *argv[])
+{
+	std::cout << "Hello, World!" << std::endl;
 	printf("Hello, World!\n");
 	return 0;
 }
